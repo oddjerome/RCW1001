eb5e03d26e4e38bfb29d8dcc407b9d3801650b36
Odd  Malael Jerome <omjerome10@live.com>
2025-06-03 11:33:55 -0400

    ajout fastApi et Dash_app

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import requests
 
-url='http://localhost:8000'
+#url='http://localhost:8000/test'
+url='https://rcw-eghffbfkfag9fadq.canadacentral-01.azurewebsites.net/'
 
 response = requests.get(url)
 response = response.json()
