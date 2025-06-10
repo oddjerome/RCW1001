08b5ebe9065d9af304306c8f30450c27c1d6f47d
Odd  Malael Jerome <omjerome10@live.com>
2025-06-10 10:31:04 -0400

    delete main et client

--- client.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-#url='http://localhost:8000/test'
-url='https://rcw-eghffbfkfag9fadq.canadacentral-01.azurewebsites.net/'
-
-response = requests.get(url)
-response = response.json()
-print(response['message'])
